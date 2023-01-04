7fb31bd435a10a142e7649df2afdef7a77abc4ff
Samuel Gomez <samuelgomez4010@gmail.com>
2023-01-03 21:21:01 -0500

    Front end updated to prevent user from typing special characters

--- index.js
+++ index.js
@@ -25,19 +25,53 @@ function decrypt(sentence) {
     .replace(/ai|enter|imes|ober|ufat/g, (letter) => decryption[letter]);
   return newSentence;
 }
+
 let message = document.getElementById("message");
+
+message.addEventListener("keypress", (e) => {
+  if (!e.key.match(/[a-z\s]/)) {
+    e.preventDefault();
+  }
+});
+
+message.addEventListener("paste", (e) => {
+  if (!/[a-z\s]/.test(e.clipboardData.getData("text"))) {
+    e.preventDefault();
+  }
+  // if (!e.clipboardData.getData('text').match(/[a-z\s]/)){
+  //   e.preventDefault();
+  // }
+});
 
 let encryptButton = document.getElementById("encrypt");
 encryptButton.addEventListener("click", () => {
-  message.value = encrypt(message.value);
+  if (!message.value) {
+    message.value =
+      "it is kind of hard to encrypt a message that does not exist mmm seems like it exists now though ";
+  } else {
+    message.value = encrypt(message.value);
+  }
 });
 
 let decryptButton = document.getElementById("decrypt");
 decryptButton.addEventListener("click", () => {
-  message.value = decrypt(message.value);
+  if (!message.value) {
+    message.value = "i wonder what the decryption of an empty message is";
+  } else {
+    message.value = decrypt(message.value);
+  }
 });
 
 let copyButton = document.getElementById("copy");
 copyButton.addEventListener("click", () => {
-  navigator.clipboard.writeText(message.value);
+  if (!message.value) {
+    navigator.clipboard.writeText("Nothing");
+  } else {
+    navigator.clipboard.writeText(message.value);
+  }
+});
+
+let resetButton = document.getElementById("reset");
+resetButton.addEventListener("click", () => {
+  message.value = "";
 });
